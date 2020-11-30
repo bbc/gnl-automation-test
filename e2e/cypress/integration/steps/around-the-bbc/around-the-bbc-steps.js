d06f9b9460c750d87442d9fc0a9a4720bd925885
Brayow1 <oliver.bray@bbc.com>
2020-11-30 09:48:17 +0000

    removed placeholders

--- e2e/cypress/integration/steps/around-the-bbc/around-the-bbc-steps.js
+++ e2e/cypress/integration/steps/around-the-bbc/around-the-bbc-steps.js
@@ -1,18 +1,18 @@
 import {Given, Then, When} from 'cypress-cucumber-preprocessor/steps';
 
-Given(/^test I visit (.*)$/, (url) => {
+Given(/^I visit (.*)$/, (url) => {
     cy.visit(url);
 });
 
-Given(/test I set the view to (.*)$/, (view) => {
+Given(/I set the view to (.*)$/, (view) => {
     cy.viewport(1280, 720);
 });
 
-When(/^test I scroll to the footer$/, () => {
+When(/^I scroll to the footer$/, () => {
     cy.get('footer').scrollIntoView();
 });
 
-Then(/^test check the around the bbc section is present$/, () => {
+Then(/^check the around the bbc section is present$/, () => {
     return cy.get('.around-the-bbc-swimlane').should('be.visible');
 });
 
