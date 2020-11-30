075c9ebc1f40c353d11ce682b917ae31b8169ef8
Brayow1 <oliver.bray@bbc.com>
2020-11-30 10:54:25 +0000

    added step definitions

--- src/test/java/com/bbc/selenium/steps/aroundthebbc/AroundTheBBCSteps.java
+++ src/test/java/com/bbc/selenium/steps/aroundthebbc/AroundTheBBCSteps.java
@@ -1,18 +1,28 @@
 package com.bbc.selenium.steps.aroundthebbc;
 
+import com.bbc.selenium.utility.ScenarioContext;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.Dimension;
 
 public class AroundTheBBCSteps {
-    @Given("^I visit future")
-    public void visitFuture() {
 
+    ScenarioContext scenarioContext = ScenarioContext.getInstanceOfScenarioContextClass();
+
+    @Given("^I open a (.*) browser")
+    public void openFirefox(String browser) {
+        scenarioContext.startBrowser(browser);
     }
 
-    @Given("^I set the view to DESKTOP")
-    public void setViewToDesktop() {
+    @Given("I resize the window to (\\d+) x (\\d+)$")
+    public void thisIsATest(int x, int y) {
+        scenarioContext.getDriver().manage().window().setSize(new Dimension(x, y));
+    }
 
+    @Given("^I navigate to the future index page$")
+    public void navigateToFutureIndexPage() {
+        scenarioContext.getDriver().get("https://www.bbc.com/future");
     }
 
     @When("^I scroll to the footer$")
@@ -28,5 +38,12 @@ public class AroundTheBBCSteps {
     @Then("^I check link (.*) on the around the bbc module works correctly$")
     public void assertLinkWorks(int link) {
 
+    }
+
+    @Then("^I close the browser$")
+    public void teardown() {
+        if (scenarioContext.getDriver() != null) {
+            scenarioContext.getDriver().quit();
+        }
     }
 }
--- src/test/resources/features/around-the-bbc/around-the-bbc.feature
+++ src/test/resources/features/around-the-bbc/around-the-bbc.feature
@@ -1,8 +1,10 @@
 Feature: Around The BBC Tests
 
   Scenario: Check links in the Around The BBC section
-    Given I visit future
-    And I set the view to DESKTOP
+    Given I open a firefox browser
+    And I resize the window to 1280 x 1000
+    And I navigate to the future index page
     When I scroll to the footer
     Then the around the bbc section is present
     And I check link 1 on the around the bbc module works correctly
+    And I close the browser
